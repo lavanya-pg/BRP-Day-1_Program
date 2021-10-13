94f9bba1abc31b88beb7a602121694520f6f1645
lavanya-pg <lavanyapg8@gmail.com>
2021-10-13 09:42:16 +0530

    Added: Day15 programs has been added.

---
+++ Day15/src/Regex/UserDetails.java
@@ -0,0 +1,43 @@
+package Regex;
+
+public class UserDetails {
+	private String firstName;
+	private String lastName;
+	private String mobileNo;
+	private String date;
+
+	public UserDetails() {
+	}
+
+	public void setfName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setlname(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setmobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public void setdate(String date) {
+		this.date = date;
+	}
+
+	public String getfName() {
+		return firstName;
+	}
+
+	public String getlastname() {
+		return lastName;
+	}
+
+	public String getmobileNo() {
+		return mobileNo;
+	}
+
+	public String getdate() {
+		return date;
+	}
+}
